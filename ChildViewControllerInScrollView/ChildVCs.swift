cb5c11e684d31766367117b8604e91c1972e662b
DonMag <ACP@ACPs-iMac.local>
2018-02-23 09:36:21 -0500

    Added code in SBOnly / Second VC to demonstrate auto-layout handling a change in content size

--- ChildViewControllerInScrollView/ChildVCs.swift
+++ ChildViewControllerInScrollView/ChildVCs.swift
@@ -23,8 +23,19 @@ class DMFirstViewController: UIViewController {
 
 class DMSecondViewController: UIViewController {
 	
+	@IBOutlet var theProofLabel: UILabel!
+	
 	@IBAction func didPressTop(_ sender: Any) {
 		print(type(of: self), #function)
+		
+		var s = "\n"
+		for i in 1...5 {
+			s += "\nAdded Line \(i)"
+		}
+		if let t = theProofLabel.text {
+			theProofLabel.text = t + s
+		}
+		
 	}
 	@IBAction func didPressMiddle(_ sender: Any) {
 		print(type(of: self), #function)
